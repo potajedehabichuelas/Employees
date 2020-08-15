d8a10e4c335a3bbb80c53a09d6222bcb327ec4a3
daniB <daniel.bolivar@glancepay.com>
2020-08-15 14:48:51 +0200

    Initial Commit

---
+++ Employees/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Employees
+//
+//  Created by Daniel Bolivar Herrera on 15/08/2020.
+//  Copyright © 2020 Daniel Bolivar Herrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
